05a29ad261ad22ef5557f1eb73c1c11c3b79385c
manikandanmohan10 <105213264+manikandanmohan10@users.noreply.github.com>
2022-10-21 16:10:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = "mongodb+srv://<username>:<password>@cluster0.kqcsrm4.mongodb.net/flask_mongodb?retryWrites=true&w=majority"
+app.config['MONGO_URI'] = "mongodb+srv://<USERNAME>:<PASSWORD>@cluster0.kqcsrm4.mongodb.net/<DATABASE_NAME>?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 todos = mongo.db.todos
